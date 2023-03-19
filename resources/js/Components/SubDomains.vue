bcf6148009ad47564ce49fed121d05fd3618f0f1
Cayvogikonyo <kevohm.km@gmail.com>
2023-03-19 11:48:33 +0300

    hide standards where both code and description is empty, hide sub-domains where name is unassigned

--- resources/js/Components/SubDomains.vue
+++ resources/js/Components/SubDomains.vue
@@ -1,22 +1,27 @@
 <template>
   <dl class="space-y-4 mx-6 my-6">
-    <Disclosure as="div" v-for="(sub_domain, index) in subDomains" :key="index" v-slot="{ open }">
-      <dt>
-        <DisclosureButton class="flex w-full text-left text-gray-900">
-          <span class="mr-6 flex h-7 items-center">
-            <PlusSmallIcon v-if="!open" class="h-6 w-6" aria-hidden="true" />
-            <MinusSmallIcon v-else class="h-6 w-6" aria-hidden="true" />
-          </span>
-          <span class="text-base font-semibold leading-7">{{ sub_domain.name }}</span>
-        </DisclosureButton>
-      </dt>
-      <transition enter-active-class="transition duration-500 ease-out" enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-out"
-        leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
-        <DisclosurePanel as="dd" class="pr-12">
-          <standards :standards="sub_domain.standards" />
-        </DisclosurePanel>
-      </transition>
+    <Disclosure as="div"  v-for="(sub_domain, index) in subDomains" :key="index" v-slot="{ open }">
+      <div v-if="sub_domain.name != ''">
+        <dt>
+          <DisclosureButton class="flex w-full text-left text-gray-900">
+            <span class="mr-6 flex h-7 items-center">
+              <PlusSmallIcon v-if="!open" class="h-6 w-6" aria-hidden="true" />
+              <MinusSmallIcon v-else class="h-6 w-6" aria-hidden="true" />
+            </span>
+            <span class="text-base font-semibold leading-7">{{ sub_domain.name }}</span>
+          </DisclosureButton>
+        </dt>
+        <transition enter-active-class="transition duration-500 ease-out" enter-from-class="transform scale-95 opacity-0"
+          enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-out"
+          leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
+          <DisclosurePanel as="dd" class="pr-12">
+            <standards :standards="sub_domain.standards" />
+          </DisclosurePanel>
+        </transition>
+      </div>
+      <div v-else>
+        <span>This category seems empty</span>
+      </div>
     </Disclosure>
   </dl>
 </template>
